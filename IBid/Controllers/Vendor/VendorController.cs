59d9d93da05b4f986c44db60c27fb607d17b0683
ahmednagy <ahmed_nagy1991@hotmail.com>
2016-12-26 11:48:31 -0800

    https://github.com/ahmednagy1991/IBid.git

---
+++ IBid/Controllers/Vendor/VendorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IBid.Controllers.Vendor
+{
+    public class VendorController : Controller
+    {
+        // GET: Vendor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateVendor()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ IBid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IBid.Startup))]
+namespace IBid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
